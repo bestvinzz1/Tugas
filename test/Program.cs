d822c20506209315f0ba8175a92874110f5659d5
herlianzhang <herlianzhang370@gmail.com>
2019-11-03 09:05:29 +0700

    test confict

--- test/Program.cs
+++ test/Program.cs
@@ -8,7 +8,7 @@ namespace test {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("hello wo");
+            Console.WriteLine("wo");
         }
     }
 }
