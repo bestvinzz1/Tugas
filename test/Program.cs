f8c118825bab18e5a5620b30c2af46e1a775c2d8
tonilim <tonilim31@rocketmail.com>
2019-11-09 23:42:05 +0700

    Tambahan Input(Tapi blom complete) Bagian pengkoreksian input(belum sempurna) dan login(Blom ada)

--- test/Program.cs
+++ test/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace test {
     class Program {
         static void Main(string[] args) {
-            string Id, Pass;
             Console.WriteLine("M    M  III   CCC  RRRR    OOOO ");
             Console.WriteLine("MM  MM   I   CC    R   R  O    O");
             Console.WriteLine("M MM M   I   C     RRRR   O    O");
@@ -19,19 +18,48 @@ namespace test {
             Console.WriteLine("   PPPP    A   A     YYY     ");
             Console.WriteLine("   P      AAAAAAA     Y      ");
             Console.WriteLine("   P     AA     AA    Y      ");
+        
+            //Penginputan
+
+            string Id, Pass;
+            int Coba = 0;
             Console.Write("\n\n++++++++++++++++++++++++++++++++++++++++");
             Console.Write("\n+");
-            Console.Write("\n+  Masukan ID dan Password anda");
-            Console.SetCursorPosition(39, 14); Console.Write("+");
-            Console.SetCursorPosition(39, 15); Console.Write("+");
-            Console.SetCursorPosition(39, 16); Console.Write("+");
-            Console.SetCursorPosition(39, 17); Console.Write("+");
-            Console.SetCursorPosition(39, 18); Console.Write("+");
-            Console.SetCursorPosition(39, 15); Console.Write("\n+    ID       :"); 
-            Console.SetCursorPosition(39, 16); Console.Write("\n+    Password : ");
-            Console.SetCursorPosition(39, 17); Console.Write("\n+\n++++++++++++++++++++++++++++++++++++++++");
-            Console.SetCursorPosition(15, 16); Id = Convert.ToString(Console.ReadLine());
-            Console.SetCursorPosition(15, 17); Pass = Convert.ToString(Console.ReadLine());
+            Console.Write("\n+    Masukan ID dan Password anda");
+            for(int i = 0; i < 9; i++)
+            {
+                Console.SetCursorPosition(39, i+14); Console.Write("+");
+            }
+            Console.SetCursorPosition(39, 15); Console.Write("\n+      ID       :"); 
+            Console.SetCursorPosition(39, 16); Console.Write("\n+      Password : ");
+            Console.SetCursorPosition(39, 17); Console.Write("\n+");
+            Console.SetCursorPosition(39, 18); Console.Write("\n+");
+            Console.SetCursorPosition(39, 19); Console.Write("\n+");
+            Console.SetCursorPosition(39, 20); Console.Write("\n+\n++++++++++++++++++++++++++++++++++++++++");
+            do
+            {
+                try
+                {
+                    Console.SetCursorPosition(18, 16); Id = Convert.ToString(Console.ReadLine());
+                    Console.SetCursorPosition(18, 17); Pass = Convert.ToString(Console.ReadLine());
+                    if (Id == "Admin" && Pass == "Admin")
+                    {
+                        Console.SetCursorPosition(15, 19); throw new Exception("Id dan Password Salah");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (Id == "Admin" && Pass == "Admin");
+ 
+ 
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
